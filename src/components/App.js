f293f3ae0e03901f2aeec0d5766b170786a42fdb
Khushi Sharma <sharmakhushi0313@gmail.com>
2024-09-02 02:16:22 -0500

    Added both preview and form components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Header from './Header'
+import Form from "./form";
+import Preview from './preview';
+
 
 const App = () => {
   return (
   <div className= "App">
     <Header Dark={true}>E-card Maker</Header>
+    <main>
+      <Form />
+      <Preview />
+    </main>
   </div>
   );
 };
