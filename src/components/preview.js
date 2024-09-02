312fdf906425093b7732770255d794e0f9f2d1b7
Khushi Sharma <sharmakhushi0313@gmail.com>
2024-09-02 05:49:44 -0500

    Created three buttons for saving cards

--- src/components/preview.js
+++ src/components/preview.js
@@ -6,8 +6,13 @@ const preview = ({Values}) => {
     return( 
     <section className = "Preview">
         <h2>Preview Pane</h2>
-        <div className="Preview-wrapper">
+        <div className="Preview-Wrapper">
             <BusinessCard Values={Values} />
+        </div>
+        <div className="Preview-Actions">
+            <button>Save as PDF</button>
+            <button>Save as PNG</button>
+            <button>Save as JPEG</button>
         </div>
       
     </section>
