895d473f61a8f4ed0450174ec53cc45d2f77b237
Khushi Sharma <sharmakhushi0313@gmail.com>
2024-09-01 20:41:35 -0500

    Styling and font changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
 const App = () => {
-  return <div className= "App">Hello</div>;
+  return <div className= "App">Hello
+
+  <header>
+    <h1><a href= "">Logo</a></h1>
+    <nav>
+      <ul>
+        <li><a href="">Link 1</a></li>
+        <li><a href="">Link 2</a></li>
+        <li><a href="">Link 3</a></li>
+        <li><a href="">Link 4</a></li>
+      </ul>
+    </nav>
+  </header>
+
+
+  </div>
   
 }
 
