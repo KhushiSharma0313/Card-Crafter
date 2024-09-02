703cc2f47893af0e18ad3732ae536a8406961e17
Khushi Sharma <sharmakhushi0313@gmail.com>
2024-09-02 01:43:38 -0500

    Added header and styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,12 @@
 import React from 'react'
+import Header from './Header'
 
 const App = () => {
-  return <div className= "App">Hello
-
-  <header>
-    <h1><a href= "">Logo</a></h1>
-    <nav>
-      <ul>
-        <li><a href="">Link 1</a></li>
-        <li><a href="">Link 2</a></li>
-        <li><a href="">Link 3</a></li>
-        <li><a href="">Link 4</a></li>
-      </ul>
-    </nav>
-  </header>
-
-
+  return (
+  <div className= "App">
+    <Header Dark={true}>E-card Maker</Header>
   </div>
-  
-}
+  );
+};
 
 export default App
